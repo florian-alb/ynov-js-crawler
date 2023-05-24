12cc5010fbc4afc131ebd13dcf8186826b61dadd
rorolastico <romainauge31@gmail.com>
2023-05-24 15:28:05 +0200

    create server for lunch pages I use nodeJS

---
+++ main.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath;
+
+    // Vérifier l'URL demandée et définir le chemin du fichier en conséquence
+    if (req.url === '/' || req.url === '/homepage.html') {
+        filePath = path.join(__dirname, 'homepage.html');
+    } else if (req.url === '/resultpage.html') {
+        filePath = path.join(__dirname, 'resultpage.html');
+    } else {
+        // Si l'URL demandée ne correspond à aucun des fichiers spécifiés, retourner une erreur 404
+        res.writeHead(404);
+        res.end('Page not found!');
+        return;
+    }
+
+    // Lire le fichier correspondant au chemin spécifié
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            // En cas d'erreur lors de la lecture du fichier, retourner une erreur 500
+            res.writeHead(500);
+            res.end('An error occurred while loading the file!');
+        } else {
+            // Définir l'en-tête de la réponse avec le type de contenu approprié
+            if (filePath.endsWith('.html')) {
+                res.setHeader('Content-Type', 'text/html');
+            } else if (filePath.endsWith('.js')) {
+                res.setHeader('Content-Type', 'application/javascript');
+            } else if (filePath.endsWith('.css')) {
+                res.setHeader('Content-Type', 'text/css');
+            }
+
+            // Envoyer le contenu du fichier en réponse
+            res.writeHead(200);
+            res.end(content);
+        }
+    });
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
