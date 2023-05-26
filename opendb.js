ade6c0c6d3892a68f4b9b78c5a779339b2820e02
francois gourbal <gourbal.francois@gmail.com>
2023-05-26 14:49:33 +0200

    finition de la database SQLite, initialisation des données.

--- opendb.js
+++ opendb.js
@@ -1,29 +1,98 @@
-import sqlite3 from 'sqlite3'
-import { open} from 'sqlite'
+import sqlite3 from "sqlite3";
 
-(async () => {
+// Create a new SQLite database
+let db = new sqlite3.Database('./data.db');
 
-    const db = await open({
-        filename: './data.db',
-        drive: sqlite3.Database
+// Execute SQL statements
+const createFirstTable = `
+    CREATE TABLE IF NOT EXISTS enterprises(
+                                              Enterprises_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                              nickname TEXT NOT NULL,
+                                              profileImg TEXT NOT NULL,
+                                              location TEXT NOT NULL,
+                                              link TEXT NOT NULL UNIQUE
+    );`;
+
+ const createSecondTable = `
+    CREATE TABLE IF NOT EXISTS employee(
+        Employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        link TEXT NOT NULL UNIQUE,
+        profileImg TEXT NOT NULL,
+        nickname TEXT NOT NULL,
+        subtitle TEXT NOT NULL,
+        location TEXT NOT NULL,
+        email TEXT NOT NULL
+);`;
+
+const insertDataQueryEnterprises = `
+    INSERT OR IGNORE INTO enterprises VALUES (NULL,?,?,?,?)`;
+const insertDataQueryEmployee = `
+    INSERT OR IGNORE INTO employee VALUES (NULL,?,?,?,?,?,?)`;
+
+const selectDataQueryEnterprises = `SELECT * FROM enterprises`;
+const selectDataQueryEmployee = `SELECT * FROM employee`;
+
+// Wrap the database operations in an async function
+async function performDatabaseOperations() {
+    // Create the tables
+    await runQuery(createFirstTable);
+    await runQuery(createSecondTable);
+
+    const dataEnterprises = {
+        nickname: 'Sample Nickname',
+        profileImg: 'sample.jpg',
+        location: 'Sample Location',
+        link: 'sample-link',
+    };
+
+    const dataEmployee = {
+        link: 'sample-link',
+        profileImg: 'sample.jpg',
+        nickname: 'Sample Name',
+        subtitle: 'sample subtitle',
+        location: 'Sample Location',
+        email: 'sample-email',
+    };
+
+    // Insert data into enterprises table
+    await runQuery(insertDataQueryEnterprises, Object.values(dataEnterprises));
+
+    // Insert data into employee table
+    await runQuery(insertDataQueryEmployee, Object.values(dataEmployee));
+
+    // Query data from enterprises table
+    const resultEnterprises = await runQuery(selectDataQueryEnterprises);
+
+    // Query data from employee table
+    const resultEmployee = await runQuery(selectDataQueryEmployee);
+
+    // Handle the queried data
+    console.log("Enterprises:", resultEnterprises);
+    console.log("Employee:", resultEmployee);
+}
+
+// Helper function to run a query with optional parameters
+function runQuery(query, params = []) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, function (error) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(this);
+            }
+        });
+    });
+}
+
+// Call the function to perform the database operations
+performDatabaseOperations()
+    .then(() => {
+        console.log("Database operations completed successfully.");
+        // Close the database connection
+        db.close();
     })
-    db.exec(`
-        CREATE TABLE IF NOT EXISTS enterprises(
-            nickname TEXT NOT NULL,
-            location TEXT NOT NULL,
-            profileImg TEXT NOT NULL,
-            link TEXT NOT NULL,
-        );
-
-        CREATE TABLE IF NOT EXISTS employee(
-            link TEXT NOT NULL UNIQUE,
-            profileImg TEXT NOT NULL,
-            nickname TEXT NOT NULL,
-            subtitle TEXT NOT NULL,
-            location TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE ,
-            tel TEXT NOT NULL UNIQUE 
-);
-`
-    )
-})()
+    .catch((error) => {
+        console.error("Error performing database operations:", error);
+        // Close the database connection in case of error
+        db.close();
+    });
